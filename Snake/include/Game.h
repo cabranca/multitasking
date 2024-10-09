850a99e65f049baeab7f6e3f3e50467b3bf7f55c
cabranca <joaquincabrera98@gmail.com>
2024-10-09 00:21:54 -0300

    Snake: added fruit, movement, collision.

--- Snake/include/Game.h
+++ Snake/include/Game.h
@@ -25,15 +25,23 @@ namespace snake {
 		void loadTextures();
 		void update(float delta);
 		void sUserInput();
+		void sMovement(float delta);
 		void sCollision();
 		void sRender();
+
+		void setBackground();
+		void spawnSnake();
+		void spawnFruit();
 
 		const uint16_t c_WindowWidth = 1280;
 		const uint16_t c_WindowHeight = 720;
 		const glm::vec2 c_SnakeSpriteSize = glm::vec2(40.0f, 40.0f);
+		const glm::vec2 c_FruitSpriteSize = glm::vec2(40.0f, 40.0f);
 		const std::string c_SpriteLabel = "sprite";
 		const std::string c_SnakeLabel = "snake";
+		const std::string c_FruitLabel = "fruit";
 		const std::string c_BackgroundLabel = "background";
+		const float c_SnakeSpeed = 200.0f;
 
 		GameState m_CurrentState;
 		Window m_Window;
--- Snake/src/Game.cpp
+++ Snake/src/Game.cpp
@@ -1,6 +1,8 @@
 #include <Game.h>
 #include <CabrankEngine.h>
 #include <ResourceManager.h>
+#include <CollisionSolver.h>
+#include <random>
 
 using namespace glm;
 using namespace cabrankengine;
@@ -50,9 +52,9 @@ void Game::init()
 	auto shader = ResourceManager::getShader(c_SpriteLabel);
 	m_Renderer = std::make_unique<SpriteRenderer>(shader);
 
-	// Create the background
-	auto background = m_EntityManager.addEntity(c_BackgroundLabel);
-	background->addComponent<CTexture>(c_BackgroundLabel, vec2(), vec2(c_WindowWidth, c_WindowHeight), vec3(1.0));
+	setBackground();
+	spawnSnake();
+	spawnFruit();
 }
 
 void Game::loadShaders()
@@ -66,6 +68,7 @@ void Game::loadShaders()
 void Game::loadTextures()
 {
 	ResourceManager::loadTexture("textures/awesomeface.png", true, c_SnakeLabel);
+	ResourceManager::loadTexture("textures/block.png", true, c_FruitLabel);
 	ResourceManager::loadTexture("textures/background.jpg", false, c_BackgroundLabel);
 }
 
@@ -73,6 +76,7 @@ void Game::update(float delta)
 {
 	m_EntityManager.update();
 	sUserInput();
+	sMovement(delta);
 	sCollision();
 	sRender();
 }
@@ -102,11 +106,68 @@ void Game::sUserInput()
 
 	if (m_InputManager.isKeyJustPressed(SDLK_ESCAPE))
 		m_CurrentState = QUIT;
+
+	for (auto& e : m_EntityManager.getEntities(c_SnakeLabel))
+	{
+		/*
+			This must only set the Input Component.
+			If a direction is set, the other ones are disabled.
+			If a direction is set, the opposite cannot be set next.
+		*/
+
+		auto newInput = CInput();
+		auto& cInput = e->getComponent<CInput>();
+
+		if (!cInput.right && m_InputManager.isKeyJustPressed(SDLK_a))
+			newInput.left = true;
+		else if (!cInput.left && m_InputManager.isKeyJustPressed(SDLK_d))
+			newInput.right = true;
+		else if (!cInput.down && m_InputManager.isKeyJustPressed(SDLK_w))
+			newInput.up = true;
+		else if (!cInput.up && m_InputManager.isKeyJustPressed(SDLK_s))
+			newInput.down = true;
+		else
+			return;
+
+		cInput = newInput;
+	}
+}
+
+void Game::sMovement(float delta)
+{
+	for (auto& e : m_EntityManager.getEntities(c_SnakeLabel))
+	{
+		auto& cInput = e->getComponent<CInput>();
+		if (cInput.left)
+			e->getComponent<CTransform>().velocity = vec2(-1.0f, 0.0f);
+		else if (cInput.right)
+			e->getComponent<CTransform>().velocity = vec2(1.0f, 0.0f);
+		else if (cInput.up)
+			e->getComponent<CTransform>().velocity = vec2(0.0f, -1.0f);
+		else if (cInput.down)
+			e->getComponent<CTransform>().velocity = vec2(0.0f, 1.0f);
+
+		e->getComponent<CTransform>().pos += e->getComponent<CTransform>().velocity * c_SnakeSpeed * delta;
+	}
 }
 
 void Game::sCollision()
 {
-
+	for (auto& x : m_EntityManager.getEntities(c_SnakeLabel))
+	{
+		for (auto& y: m_EntityManager.getEntities(c_FruitLabel))
+		{
+			if (x->hasComponent<CTransform>() && x->hasComponent<CBoundingBox>() && y->hasComponent<CTransform>() && y->hasComponent<CBoundingBox>())
+			{
+				if (CollisionSolver::AABBCollision(x, y))
+				{
+					y->destroy();
+					spawnFruit();
+				}
+			}
+				
+		}
+	}
 }
 
 void Game::sRender()
@@ -120,9 +181,39 @@ void Game::sRender()
 		{
 			auto& cTexture = entity->getComponent<CTexture>();
 			auto texture = ResourceManager::getTexture(cTexture.name);
-			m_Renderer->drawSprite(texture, cTexture.pos, cTexture.size, 0.0f, cTexture.color);
+			m_Renderer->drawSprite(texture, entity->getComponent<CTransform>().pos, cTexture.size, 0.0f, cTexture.color);
 		}
 	}
 	
 	m_Window.swapBuffer();
+}
+
+void Game::setBackground()
+{
+	auto background = m_EntityManager.addEntity(c_BackgroundLabel);
+	background->addComponent<CTexture>(c_BackgroundLabel, vec2(), vec2(c_WindowWidth, c_WindowHeight), vec3(1.0));
+}
+
+void Game::spawnSnake()
+{
+	auto snake = m_EntityManager.addEntity(c_SnakeLabel);
+	snake->addComponent<CTransform>(vec2(c_WindowWidth / 2, c_WindowHeight / 2), vec2(), 0);
+	snake->addComponent<CBoundingBox>(c_SnakeSpriteSize);
+	snake->addComponent<CTexture>(c_SnakeLabel, vec2(c_WindowWidth / 2, c_WindowHeight / 2), c_SnakeSpriteSize, vec3(1.0f));
+}
+
+void Game::spawnFruit()
+{
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_int_distribution<std::mt19937::result_type> randx(0, c_WindowWidth - c_FruitSpriteSize.x);
+	std::uniform_int_distribution<std::mt19937::result_type> randy(0, c_WindowHeight - c_FruitSpriteSize.y);
+
+	auto x = randx(rng);
+	auto y = randy(rng);
+
+	auto fruit = m_EntityManager.addEntity(c_FruitLabel);
+	fruit->addComponent<CTransform>(vec2(x, y), vec2(), 0);
+	fruit->addComponent<CBoundingBox>(c_FruitSpriteSize);
+	fruit->addComponent<CTexture>(c_FruitLabel, vec2(x, y), c_FruitSpriteSize, vec3(1.0f));
 }
