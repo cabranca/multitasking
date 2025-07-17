93473f3c0978449c05ee9fdec6d89938b52525dc
cabranca <joaquincabrera98@gmail.com>
2025-07-16 22:54:02 -0300

    Moved Application class

---
+++ CabrankEngine/src/Core/Application.cpp
@@ -0,0 +1,20 @@
+#include "Application.h"
+
+namespace cabrankengine {
+	Application::Application()
+	{
+	}
+
+	Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (true) {
+
+		}
+	}
+}
+
+
---
+++ CabrankEngine/src/Core/Application.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace cabrankengine {
+
+	class Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+	private:
+		void Run();
+	};
+
+}
