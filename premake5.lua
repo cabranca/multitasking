4ada3256d06ad707089e564ed83ae5969d931787
cabranca <joaquincabrera98@gmail.com>
2025-07-19 10:24:54 -0300

    Added premake file for Windows config

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+workspace "Cabrankengine"
+    architecture "x64"
+
+    configurations {"Debug", "Release"}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Cabrankengine"
+    location "Cabrankengine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+    includedirs {"%{prj.name}/vendor/spdlog/include"}
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {"CE_PLATFORM_WINDOWS", "CE_BUILD_DLL"}
+        postbuildcommands {("{COPYFILE} %{cfg.buildtarget.relpath} .. /bin/" .. outputdir .. "/Sandbox")}
+    
+    filter "configurations:Debug"
+        defines "CE_DEBUG"
+        symbols "On"
+    filter "configurations:Release"
+        defines "CE_RELEASE"
+        symbols "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+    includedirs {"%{prj.name}/vendor/spdlog/include", "Cabrankengine/src"}
+    links {"Cabrankengine"}
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {"CE_PLATFORM_WINDOWS"}
+        
+    filter "configurations:Debug"
+        defines "CE_DEBUG"
+        symbols "On"
+    filter "configurations:Release"
+        defines "CE_RELEASE"
+        symbols "On"
