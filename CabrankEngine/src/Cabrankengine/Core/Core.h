c2765ed54a209feace6e1df6d6946c57bdc1cc58
cabranca <joaquincabrera98@gmail.com>
2025-07-20 17:52:22 -0300

    Added asserts.

--- CabrankEngine/src/Cabrankengine/Core/Core.h
+++ CabrankEngine/src/Cabrankengine/Core/Core.h
@@ -12,4 +12,12 @@
 	#error Cabrankengine only supports Windows!
 #endif
 
+#ifdef CE_ENABLE_ASSERTS
+	#define CE_ASSERT(x, ...) { if(!(x)) { CE_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define CE_CORE_ASSERT(x, ...) { if(!(x)) { CE_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define CE_ASSERT(x, ...)
+	#define CE_CORE_ASSERT(x, ...)
+#endif
+
 constexpr uint32_t bit(int x) { return 1u << x; }
