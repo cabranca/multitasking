44abce81f327fc9b7daab7a988f70689588be01d
cabranca <joaquincabrera98@gmail.com>
2025-07-20 17:52:48 -0300

    Added Windows' Window implementation.

---
+++ CabrankEngine/src/Platform/Windows/WindowsWindow.cpp
@@ -0,0 +1,60 @@
+#include "WindowsWindow.h"
+
+#include <Cabrankengine.h>
+
+namespace cabrankengine {
+	static bool s_GLFWInitialized = false;
+
+	Window* Window::create(const WindowProps& props) {
+		return new WindowsWindow(props);
+	}
+	
+	WindowsWindow::WindowsWindow(const WindowProps& props) {
+		init(props);
+	}
+
+	WindowsWindow::~WindowsWindow() {
+		shutdown();
+	}
+
+	void WindowsWindow::onUpdate() {
+		glfwPollEvents();
+		glfwSwapBuffers(m_Window);
+	}
+
+	void WindowsWindow::setVSync(bool enabled) {
+		if (enabled)
+			glfwSwapInterval(1);
+		else
+			glfwSwapInterval(0);
+		m_Data.VSync = enabled;
+	}
+
+	bool WindowsWindow::isVSync() const {
+		return m_Data.VSync;
+	}
+
+	void WindowsWindow::init(const WindowProps& props) {
+		m_Data.Title = props.Title;
+		m_Data.Width = props.Width;
+		m_Data.Height = props.Height;
+
+		CE_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
+
+		if (!s_GLFWInitialized) {
+			int success = glfwInit();
+			CE_CORE_ASSERT(success, "Could not initialize GLFW!");
+
+			s_GLFWInitialized = true;
+		}
+
+		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+		glfwMakeContextCurrent(m_Window);
+		glfwSetWindowUserPointer(m_Window, &m_Data);
+		setVSync(true);
+	}
+
+	void WindowsWindow::shutdown() {
+		glfwDestroyWindow(m_Window);
+	}
+}
