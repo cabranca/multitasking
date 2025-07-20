5c475ca86af26024bbe8f739198d22d58a8230f8
cabranca <joaquincabrera98@gmail.com>
2025-07-20 16:33:37 -0300

    Created Event system.

---
+++ CabrankEngine/src/Cabrankengine/Events/Event.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <string>
+#include "../Core/Core.h"
+
+namespace cabrankengine {
+
+	enum class EventType {
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased, KeyTyped,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory {
+		None = 0,
+		EventCategoryApplication = bit(0),
+		EventCategoryInput = bit(1),
+		EventCategoryKeyboard = bit(2),
+		EventCategoryMouse = bit(3),
+		EventCategoryMouseButton = bit(4)
+	};
+
+#define EVENT_CLASS_TYPE(type) static EventType getStaticType() { return EventType::type; }\
+								virtual EventType getEventType() const override { return getStaticType(); }\
+								virtual const char* getName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int getCategoryFlags() const override { return category; }
+
+	class Event
+	{
+	public:
+		virtual ~Event() = default;
+
+		bool Handled = false;
+
+		virtual EventType getEventType() const = 0;
+		virtual const char* getName() const = 0;
+		virtual int getCategoryFlags() const = 0;
+		virtual std::string toString() const { return getName(); }
+
+		inline bool isInCategory(EventCategory category) const { return getCategoryFlags() & category; }
+	};
+
+	class EventDispatcher
+	{
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event)
+		{
+		}
+
+		template<typename T, typename F>
+		bool dispatch(const F& func)
+		{
+			if (m_Event.getEventType() == T::getStaticType())
+			{
+				m_Event.Handled |= func(static_cast<T&>(m_Event));
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.toString();
+	}
+
+}
