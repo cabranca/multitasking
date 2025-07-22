6c2eb39dcc8bc0a4a94b0f83984208811a5c04c2
cabranca <joaquincabrera98@gmail.com>
2025-07-22 19:21:25 -0300

    Included ImGuiLayer in Cabrankengine header

--- Cabrankengine/src/Cabrankengine.h
+++ Cabrankengine/src/Cabrankengine.h
@@ -4,6 +4,7 @@
 #include "Cabrankengine/Core/Application.h"
 #include "Cabrankengine/Core/Logger.h"
 #include "Cabrankengine/Core/Layer.h"
+#include "Cabrankengine/ImGui/ImGuiLayer.h"
 
 // --- Entry Point ---
 #include "Cabrankengine/Core/EntryPoint.h"
