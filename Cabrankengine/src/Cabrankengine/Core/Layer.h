89057a6816dce6083ae520977da961dc61f7314b
cabranca <joaquincabrera98@gmail.com>
2025-07-22 17:38:57 -0300

    Fixed return value

--- Cabrankengine/src/Cabrankengine/Core/Layer.h
+++ Cabrankengine/src/Cabrankengine/Core/Layer.h
@@ -15,7 +15,7 @@ namespace cabrankengine {
 		virtual void onUpdate() {}
 		virtual void onEvent(Event& event) {}
 
-		inline const std::string& getName() const { return ""; }
+		inline const std::string& getName() const { return m_DebugName; }
 
 	protected:
 		std::string m_DebugName;
