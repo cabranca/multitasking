2ac97990c3c35d7d24ad5cd77f0ba4a7a6863dae
cabranca <joaquincabrera98@gmail.com>
2025-07-24 19:49:23 -0300

    Added Input interface

---
+++ Cabrankengine/src/Cabrankengine/Core/Input.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Cabrankengine/Core/Core.h>
+#include <Cabrankengine/Core/KeyCodes.h>
+#include <Cabrankengine/Core/MouseCodes.h>
+#include <tuple>
+
+namespace cabrankengine {
+
+	class CABRANKENGINE_API Input {
+	public:
+		inline static bool isKeyPressed(KeyCode keyCode) { return s_Instance->isKeyPressedImpl(keyCode); }
+
+		inline static bool isMouseButtonPressed(MouseCode mouseCode) { return s_Instance->isMouseButtonPressedImpl(mouseCode); }
+		inline static std::pair<float, float> getMousePosition() { return s_Instance->getMousePositionImpl(); }
+		inline static float getMouseX() { return s_Instance->getMouseXImpl(); }
+		inline static float getMouseY() { return s_Instance->getMouseYImpl(); }
+
+	protected:
+		virtual bool isKeyPressedImpl(KeyCode keyCode) = 0;
+		virtual bool isMouseButtonPressedImpl(MouseCode mouseCode) = 0;
+		virtual std::pair<float, float> getMousePositionImpl() = 0;
+		virtual float getMouseXImpl() = 0;
+		virtual float getMouseYImpl() = 0;
+
+	private:
+		static Input* s_Instance; //TODO: check how to initialize inline (C++17/20 feature).
+	};
+}
