3067f98f009336996b5c00b1521337f3908b2aab
cabranca <joaquincabrera98@gmail.com>
2025-07-25 00:12:41 -0300

    Udpated ImGuiLayer

--- Cabrankengine/src/Cabrankengine/ImGui/ImGuiLayer.cpp
+++ Cabrankengine/src/Cabrankengine/ImGui/ImGuiLayer.cpp
@@ -1,138 +1,13 @@
 #include <Cabrankengine/ImGui/ImGuiLayer.h>
 
-#include <Platform/OpenGL/ImGuiOpenGLRenderer.h>
-#include <GLFW/glfw3.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
 #include <Cabrankengine/Core/Application.h>
 #include <Cabrankengine/Core/Window.h>
+
+// TEMPORARY
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
-
-ImGuiKey ImGui_ImplGlfw_KeyToImGuiKey(int keycode)
-{
-    switch (keycode)
-    {
-    case GLFW_KEY_TAB: return ImGuiKey_Tab;
-    case GLFW_KEY_LEFT: return ImGuiKey_LeftArrow;
-    case GLFW_KEY_RIGHT: return ImGuiKey_RightArrow;
-    case GLFW_KEY_UP: return ImGuiKey_UpArrow;
-    case GLFW_KEY_DOWN: return ImGuiKey_DownArrow;
-    case GLFW_KEY_PAGE_UP: return ImGuiKey_PageUp;
-    case GLFW_KEY_PAGE_DOWN: return ImGuiKey_PageDown;
-    case GLFW_KEY_HOME: return ImGuiKey_Home;
-    case GLFW_KEY_END: return ImGuiKey_End;
-    case GLFW_KEY_INSERT: return ImGuiKey_Insert;
-    case GLFW_KEY_DELETE: return ImGuiKey_Delete;
-    case GLFW_KEY_BACKSPACE: return ImGuiKey_Backspace;
-    case GLFW_KEY_SPACE: return ImGuiKey_Space;
-    case GLFW_KEY_ENTER: return ImGuiKey_Enter;
-    case GLFW_KEY_ESCAPE: return ImGuiKey_Escape;
-    case GLFW_KEY_APOSTROPHE: return ImGuiKey_Apostrophe;
-    case GLFW_KEY_COMMA: return ImGuiKey_Comma;
-    case GLFW_KEY_MINUS: return ImGuiKey_Minus;
-    case GLFW_KEY_PERIOD: return ImGuiKey_Period;
-    case GLFW_KEY_SLASH: return ImGuiKey_Slash;
-    case GLFW_KEY_SEMICOLON: return ImGuiKey_Semicolon;
-    case GLFW_KEY_EQUAL: return ImGuiKey_Equal;
-    case GLFW_KEY_LEFT_BRACKET: return ImGuiKey_LeftBracket;
-    case GLFW_KEY_BACKSLASH: return ImGuiKey_Backslash;
-    case GLFW_KEY_WORLD_1: return ImGuiKey_Oem102;
-    case GLFW_KEY_WORLD_2: return ImGuiKey_Oem102;
-    case GLFW_KEY_RIGHT_BRACKET: return ImGuiKey_RightBracket;
-    case GLFW_KEY_GRAVE_ACCENT: return ImGuiKey_GraveAccent;
-    case GLFW_KEY_CAPS_LOCK: return ImGuiKey_CapsLock;
-    case GLFW_KEY_SCROLL_LOCK: return ImGuiKey_ScrollLock;
-    case GLFW_KEY_NUM_LOCK: return ImGuiKey_NumLock;
-    case GLFW_KEY_PRINT_SCREEN: return ImGuiKey_PrintScreen;
-    case GLFW_KEY_PAUSE: return ImGuiKey_Pause;
-    case GLFW_KEY_KP_0: return ImGuiKey_Keypad0;
-    case GLFW_KEY_KP_1: return ImGuiKey_Keypad1;
-    case GLFW_KEY_KP_2: return ImGuiKey_Keypad2;
-    case GLFW_KEY_KP_3: return ImGuiKey_Keypad3;
-    case GLFW_KEY_KP_4: return ImGuiKey_Keypad4;
-    case GLFW_KEY_KP_5: return ImGuiKey_Keypad5;
-    case GLFW_KEY_KP_6: return ImGuiKey_Keypad6;
-    case GLFW_KEY_KP_7: return ImGuiKey_Keypad7;
-    case GLFW_KEY_KP_8: return ImGuiKey_Keypad8;
-    case GLFW_KEY_KP_9: return ImGuiKey_Keypad9;
-    case GLFW_KEY_KP_DECIMAL: return ImGuiKey_KeypadDecimal;
-    case GLFW_KEY_KP_DIVIDE: return ImGuiKey_KeypadDivide;
-    case GLFW_KEY_KP_MULTIPLY: return ImGuiKey_KeypadMultiply;
-    case GLFW_KEY_KP_SUBTRACT: return ImGuiKey_KeypadSubtract;
-    case GLFW_KEY_KP_ADD: return ImGuiKey_KeypadAdd;
-    case GLFW_KEY_KP_ENTER: return ImGuiKey_KeypadEnter;
-    case GLFW_KEY_KP_EQUAL: return ImGuiKey_KeypadEqual;
-    case GLFW_KEY_LEFT_SHIFT: return ImGuiKey_LeftShift;
-    case GLFW_KEY_LEFT_CONTROL: return ImGuiKey_LeftCtrl;
-    case GLFW_KEY_LEFT_ALT: return ImGuiKey_LeftAlt;
-    case GLFW_KEY_LEFT_SUPER: return ImGuiKey_LeftSuper;
-    case GLFW_KEY_RIGHT_SHIFT: return ImGuiKey_RightShift;
-    case GLFW_KEY_RIGHT_CONTROL: return ImGuiKey_RightCtrl;
-    case GLFW_KEY_RIGHT_ALT: return ImGuiKey_RightAlt;
-    case GLFW_KEY_RIGHT_SUPER: return ImGuiKey_RightSuper;
-    case GLFW_KEY_MENU: return ImGuiKey_Menu;
-    case GLFW_KEY_0: return ImGuiKey_0;
-    case GLFW_KEY_1: return ImGuiKey_1;
-    case GLFW_KEY_2: return ImGuiKey_2;
-    case GLFW_KEY_3: return ImGuiKey_3;
-    case GLFW_KEY_4: return ImGuiKey_4;
-    case GLFW_KEY_5: return ImGuiKey_5;
-    case GLFW_KEY_6: return ImGuiKey_6;
-    case GLFW_KEY_7: return ImGuiKey_7;
-    case GLFW_KEY_8: return ImGuiKey_8;
-    case GLFW_KEY_9: return ImGuiKey_9;
-    case GLFW_KEY_A: return ImGuiKey_A;
-    case GLFW_KEY_B: return ImGuiKey_B;
-    case GLFW_KEY_C: return ImGuiKey_C;
-    case GLFW_KEY_D: return ImGuiKey_D;
-    case GLFW_KEY_E: return ImGuiKey_E;
-    case GLFW_KEY_F: return ImGuiKey_F;
-    case GLFW_KEY_G: return ImGuiKey_G;
-    case GLFW_KEY_H: return ImGuiKey_H;
-    case GLFW_KEY_I: return ImGuiKey_I;
-    case GLFW_KEY_J: return ImGuiKey_J;
-    case GLFW_KEY_K: return ImGuiKey_K;
-    case GLFW_KEY_L: return ImGuiKey_L;
-    case GLFW_KEY_M: return ImGuiKey_M;
-    case GLFW_KEY_N: return ImGuiKey_N;
-    case GLFW_KEY_O: return ImGuiKey_O;
-    case GLFW_KEY_P: return ImGuiKey_P;
-    case GLFW_KEY_Q: return ImGuiKey_Q;
-    case GLFW_KEY_R: return ImGuiKey_R;
-    case GLFW_KEY_S: return ImGuiKey_S;
-    case GLFW_KEY_T: return ImGuiKey_T;
-    case GLFW_KEY_U: return ImGuiKey_U;
-    case GLFW_KEY_V: return ImGuiKey_V;
-    case GLFW_KEY_W: return ImGuiKey_W;
-    case GLFW_KEY_X: return ImGuiKey_X;
-    case GLFW_KEY_Y: return ImGuiKey_Y;
-    case GLFW_KEY_Z: return ImGuiKey_Z;
-    case GLFW_KEY_F1: return ImGuiKey_F1;
-    case GLFW_KEY_F2: return ImGuiKey_F2;
-    case GLFW_KEY_F3: return ImGuiKey_F3;
-    case GLFW_KEY_F4: return ImGuiKey_F4;
-    case GLFW_KEY_F5: return ImGuiKey_F5;
-    case GLFW_KEY_F6: return ImGuiKey_F6;
-    case GLFW_KEY_F7: return ImGuiKey_F7;
-    case GLFW_KEY_F8: return ImGuiKey_F8;
-    case GLFW_KEY_F9: return ImGuiKey_F9;
-    case GLFW_KEY_F10: return ImGuiKey_F10;
-    case GLFW_KEY_F11: return ImGuiKey_F11;
-    case GLFW_KEY_F12: return ImGuiKey_F12;
-    case GLFW_KEY_F13: return ImGuiKey_F13;
-    case GLFW_KEY_F14: return ImGuiKey_F14;
-    case GLFW_KEY_F15: return ImGuiKey_F15;
-    case GLFW_KEY_F16: return ImGuiKey_F16;
-    case GLFW_KEY_F17: return ImGuiKey_F17;
-    case GLFW_KEY_F18: return ImGuiKey_F18;
-    case GLFW_KEY_F19: return ImGuiKey_F19;
-    case GLFW_KEY_F20: return ImGuiKey_F20;
-    case GLFW_KEY_F21: return ImGuiKey_F21;
-    case GLFW_KEY_F22: return ImGuiKey_F22;
-    case GLFW_KEY_F23: return ImGuiKey_F23;
-    case GLFW_KEY_F24: return ImGuiKey_F24;
-    default: return ImGuiKey_None;
-    }
-}
 
 namespace cabrankengine {
 
@@ -142,117 +17,60 @@ namespace cabrankengine {
 
 	void ImGuiLayer::onAttach()
 	{
+		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
+		
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
 		ImGui::StyleColorsDark();
 
-		ImGuiIO& io = ImGui::GetIO();
-		io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
-		io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
+		ImGuiStyle& style = ImGui::GetStyle();
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+			style.WindowRounding = 0.f;
+			style.Colors[ImGuiCol_WindowBg].w = 1.f;
+		}
 
+		auto window = static_cast<GLFWwindow*>(Application::get().getWindow().getNativeWindow());
+
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init("#version 410");
 	}
 
 	void ImGuiLayer::onDettach()
 	{
-
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
 	}
 
-	void ImGuiLayer::onUpdate()
-	{
+	void ImGuiLayer::onImGuiRender() {
+		static bool show = true;
+		ImGui::ShowDemoWindow(&show);
+	}
+
+	void ImGuiLayer::begin() {
+		ImGui_ImplGlfw_NewFrame();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui::NewFrame();
+	}
+
+	void ImGuiLayer::end() {
 		ImGuiIO& io = ImGui::GetIO();
 		Application& app = Application::get();
 		io.DisplaySize = ImVec2(app.getWindow().getWidth(), app.getWindow().getHeight());
 
-		float time = (float)glfwGetTime();
-		io.DeltaTime = m_Time > 0.0 ? (time - m_Time) : (1.f / 60.f);
-
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui::NewFrame();
-
-		static bool show = true;
-		ImGui::ShowDemoWindow(&show);
-
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-	}
 
-	void ImGuiLayer::onEvent(Event& event)
-	{
-		EventDispatcher dispatcher(event);
-		dispatcher.dispatch<MouseButtonPressedEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onMouseButtonPressedEvent));
-		dispatcher.dispatch<MouseButtonReleasedEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onMouseButtonReleasedEvent));
-		dispatcher.dispatch<MouseMovedEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onMouseMovedEvent));
-		dispatcher.dispatch<MouseScrolledEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onMouseScrolledEvent));
-		dispatcher.dispatch<KeyPressedEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onKeyPressedEvent));
-		dispatcher.dispatch<KeyTypedEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onKeyTypedEvent));
-		dispatcher.dispatch<KeyReleasedEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onKeyReleasedEvent));
-		dispatcher.dispatch<WindowResizeEvent>(CE_BIND_EVENT_FN(ImGuiLayer::onWindowResizedEvent));
-	}
-
-	bool ImGuiLayer::onMouseButtonPressedEvent(MouseButtonPressedEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		io.MouseDown[e.getMouseButton()] = true;
-
-		return false;
-	}
-
-	bool ImGuiLayer::onMouseButtonReleasedEvent(MouseButtonReleasedEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		io.MouseDown[e.getMouseButton()] = false;
-
-		return false;
-	}
-
-	bool ImGuiLayer::onMouseMovedEvent(MouseMovedEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		io.MousePos = ImVec2(e.getX(), e.getY());
-
-		return false;
-	}
-
-	bool ImGuiLayer::onMouseScrolledEvent(MouseScrolledEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		io.MouseWheelH += e.getXOffset();
-		io.MouseWheel += e.getYOffset();
-
-		return false;
-	}
-
-	bool ImGuiLayer::onKeyPressedEvent(KeyPressedEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		io.KeysData[ImGui_ImplGlfw_KeyToImGuiKey(e.getKeyCode()) - ImGuiKey_NamedKey_BEGIN].Down = true;
-
-		
-		io.KeyCtrl = io.KeyMods | ImGuiMod_Ctrl;
-		io.KeyShift = io.KeyMods | ImGuiMod_Shift;
-		io.KeyAlt = io.KeyMods | ImGuiMod_Alt;
-		io.KeySuper = io.KeyMods | ImGuiMod_Super;
-
-		return false;
-	}
-
-	bool ImGuiLayer::onKeyTypedEvent(KeyTypedEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		int keyCode = e.getKeyCode();
-		if (keyCode > 0 && keyCode < 0x10000)
-			io.AddInputCharacter((unsigned short)keyCode);
-
-		return false;
-	}
-
-	bool ImGuiLayer::onKeyReleasedEvent(KeyReleasedEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		io.KeysData[ImGui_ImplGlfw_KeyToImGuiKey(e.getKeyCode()) - ImGuiKey_NamedKey_BEGIN].Down = false;
-
-		return false;
-	}
-
-	bool ImGuiLayer::onWindowResizedEvent(WindowResizeEvent& e) {
-		ImGuiIO& io = ImGui::GetIO();
-		io.DisplaySize = ImVec2(e.getWidth(), e.getHeight());
-		io.DisplayFramebufferScale = ImVec2(1.f, 1.f);
-		glViewport(0, 0, e.getWidth(), e.getHeight());
-
-		return false;
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
 	}
 }
--- Cabrankengine/src/Cabrankengine/ImGui/ImGuiLayer.h
+++ Cabrankengine/src/Cabrankengine/ImGui/ImGuiLayer.h
@@ -1,9 +1,6 @@
 #pragma once
 
 #include <Cabrankengine/Core/Layer.h>
-#include <Cabrankengine/Events/KeyEvent.h>
-#include <Cabrankengine/Events/MouseEvent.h>
-#include <Cabrankengine/Events/ApplicationEvent.h>
 
 namespace cabrankengine {
 	
@@ -13,28 +10,17 @@ namespace cabrankengine {
 		~ImGuiLayer();
 
 		// Callback for when the layer is pushed to the stack
-		virtual void onAttach() override; // TODO: Cherno didn't make this virtual override.
+		virtual void onAttach() override;
 
 		// Callback for when the layer is popped from the stack
 		virtual void onDettach() override;
 
-		// Callback for when the layer must update
-		virtual void onUpdate() override;
+		virtual void onImGuiRender() override;
 
-		// Callback for the event system
-		virtual void onEvent(Event& event) override;
+		void begin();
+		void end();
 
 	private:
-
-		bool onMouseButtonPressedEvent(MouseButtonPressedEvent& e);
-		bool onMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
-		bool onMouseMovedEvent(MouseMovedEvent& e);
-		bool onMouseScrolledEvent(MouseScrolledEvent& e);
-		bool onKeyPressedEvent(KeyPressedEvent& e);
-		bool onKeyTypedEvent(KeyTypedEvent& e);
-		bool onKeyReleasedEvent(KeyReleasedEvent& e);
-		bool onWindowResizedEvent(WindowResizeEvent& e);
-
 		float m_Time; // Time used to calculate delta
 	};
 }
