54ebc3331f591c88ed039bff93809feaa868b956
cabranca <joaquincabrera98@gmail.com>
2025-08-03 20:45:48 -0300

    Added Vertex Array Implementation for OpenGL

---
+++ Cabrankengine/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -0,0 +1,70 @@
+#include <Platform/OpenGL/OpenGLVertexArray.h>
+
+#include <glad/glad.h>
+
+namespace cabrankengine {
+
+	static GLenum ShaderDataType2OpenGLBaseType(ShaderDataType type) {
+		switch (type) {
+			case ShaderDataType::Float:   return GL_FLOAT;
+			case ShaderDataType::Float2:  return GL_FLOAT;
+			case ShaderDataType::Float3:  return GL_FLOAT;
+			case ShaderDataType::Float4:  return GL_FLOAT;
+			case ShaderDataType::Mat3:    return GL_FLOAT;
+			case ShaderDataType::Mat4:    return GL_FLOAT;
+			case ShaderDataType::Int:     return GL_INT;
+			case ShaderDataType::Int2:    return GL_INT;
+			case ShaderDataType::Int3:    return GL_INT;
+			case ShaderDataType::Int4:    return GL_INT;
+			case ShaderDataType::Bool:    return GL_BOOL;
+		}
+
+		CE_CORE_ASSERT(false, "Unknown Shader Type!");
+		return 0;
+	}
+
+	OpenGLVertexArray::OpenGLVertexArray() : m_RendererId() {
+		glCreateVertexArrays(1, &m_RendererId);
+	}
+
+	OpenGLVertexArray::~OpenGLVertexArray() {
+		glDeleteVertexArrays(1, &m_RendererId);
+	}
+
+	void OpenGLVertexArray::bind() const {
+		glBindVertexArray(m_RendererId);
+	}
+
+	void OpenGLVertexArray::unbind() const {
+		glBindVertexArray(0);
+	}
+
+	void OpenGLVertexArray::addVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) {
+		CE_CORE_ASSERT(vertexBuffer->getLayout().getElements().size(), "Vertex Buffer has no layout!");
+
+		glBindVertexArray(m_RendererId);
+		vertexBuffer->bind();
+
+		const auto& layout = vertexBuffer->getLayout();
+		uint32_t index = 0;
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.getComponentCount(),
+				ShaderDataType2OpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.getStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
+		m_VertexBuffers.push_back(vertexBuffer);
+	}
+
+	void OpenGLVertexArray::setIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) {
+		glBindVertexArray(m_RendererId);
+		indexBuffer->bind();
+
+		m_IndexBuffer = indexBuffer;
+	}
+}
