59816c4c2a00a35af83e7fac99dad3b9c69ed58b
cabranca <joaquincabrera98@gmail.com>
2025-08-04 01:27:40 -0300

    Updated Renderer

--- Cabrankengine/src/Cabrankengine/Renderer/Renderer.cpp
+++ Cabrankengine/src/Cabrankengine/Renderer/Renderer.cpp
@@ -1,16 +1,23 @@
 #include <Cabrankengine/Renderer/Renderer.h>
 
+#include <Cabrankengine/Renderer/OrthographicCamera.h>
+#include <Cabrankengine/Renderer/Shader.h>
+
 namespace cabrankengine {
 
-	void Renderer::beginScene() {
+	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData();
 
+	void Renderer::beginScene(OrthographicCamera& camera) {
+		s_SceneData->viewProjectionMatrix = camera.getViewProjectionMatrix();
 	}
 
 	void Renderer::endScene() {
 
 	}
 
-	void Renderer::submit(const std::shared_ptr<VertexArray>& vertexArray) {
+	void Renderer::submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray) {
+		shader->bind();
+		shader->uploadUniformMat4("u_viewProjection", s_SceneData->viewProjectionMatrix);
 		vertexArray->bind();
 		RenderCommand::drawIndexed(vertexArray);
 	}
