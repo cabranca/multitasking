091e372664f4751957c55aeaf8161a038b19b4df
cabranca <joaquincabrera98@gmail.com>
2025-08-04 01:27:19 -0300

    Added orthographic camera

---
+++ Cabrankengine/src/Cabrankengine/Renderer/OrthographicCamera.cpp
@@ -0,0 +1,17 @@
+#include <Cabrankengine/Renderer/OrthographicCamera.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace cabrankengine {
+
+	OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top)
+		: m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.f, 1.f)), m_ViewMatrix(1.f), 
+		  m_ViewProjectionMatrix(m_ProjectionMatrix * m_ViewMatrix), m_Position(), m_Rotation() {}
+
+	void OrthographicCamera::recalculateViewMatrix() {
+		glm::mat4 translation = glm::translate(glm::mat4(1.0f), m_Position);
+		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+		m_ViewMatrix = glm::inverse(translation * rotation);
+		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+	}
+}
