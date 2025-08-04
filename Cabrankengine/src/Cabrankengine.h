c6ade5dbe0090ad73e1c72246ea696fdd5a71139
cabranca <joaquincabrera98@gmail.com>
2025-08-04 01:54:24 -0300

    Updated header file

--- Cabrankengine/src/Cabrankengine.h
+++ Cabrankengine/src/Cabrankengine.h
@@ -1,11 +1,26 @@
 #pragma once
 
 // For use by Cabrankengine Application
-#include "Cabrankengine/Core/Application.h"
-#include "Cabrankengine/Core/Logger.h"
-#include "Cabrankengine/Core/Layer.h"
-#include "Cabrankengine/ImGui/ImGuiLayer.h"
-#include "Cabrankengine/Core/Input.h"
+#include <Cabrankengine/Core/Application.h>
+#include <Cabrankengine/Core/Layer.h>
+#include <Cabrankengine/Core/Logger.h>
+
+#include <Cabrankengine/Core/Input.h>
+#include <Cabrankengine/Core/KeyCodes.h>
+#include <Cabrankengine/Core/MouseCodes.h>
+
+#include <Cabrankengine/ImGui/ImGuiLayer.h>
+
+
+// --- Renderer ---
+#include <Cabrankengine/Renderer/Renderer.h>
+#include <Cabrankengine/Renderer/RenderCommand.h>
+
+#include <Cabrankengine/Renderer/Buffer.h>
+#include <Cabrankengine/Renderer/Shader.h>
+#include <Cabrankengine/Renderer/VertexArray.h>
+
+#include <Cabrankengine/Renderer/OrthographicCamera.h>
 
 // --- Entry Point ---
 #include "Cabrankengine/Core/EntryPoint.h"
