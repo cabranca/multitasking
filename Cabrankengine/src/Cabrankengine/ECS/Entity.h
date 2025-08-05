018985ec83c8dcd4ef69acf6bc55349165d60a11
cabranca <joaquincabrera98@gmail.com>
2025-08-05 01:25:52 -0300

    Added Entity class

---
+++ Cabrankengine/src/Cabrankengine/ECS/Entity.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <Cabrankengine/ECS/Components.hpp>
+
+namespace cabrankengine
+{
+	// This MUST match all the possible components in Components.hpp
+	typedef std::tuple<
+		CPlayer,
+		CTransform,
+		CBoundingBox,
+		CLinearMovement
+	> ComponentTuple;
+
+	class Entity
+	{
+	public:
+		// Adds the given component
+		template<typename T, typename... TArgs>
+		T& addComponent(TArgs&&... mArgs)
+		{
+			auto& component = getComponent<T>();
+			component = T(std::forward<TArgs>(mArgs)...);
+			component.has = true;
+			return component;
+		}
+
+		// Sets m_Active to false
+		void destroy();
+
+		// Getters
+		size_t getId() const;
+		bool isActive() const;
+
+		template<typename T>
+		bool hasComponent() const
+		{
+			return getComponent<T>().has;
+		}
+
+		template<typename T>
+		T& getComponent()
+		{
+			return std::get<T>(m_Components);
+		}
+
+		template<typename T>
+		const T& getComponent() const
+		{
+			return std::get<T>(m_Components);
+		}
+		
+		template<typename T>
+		void removeComponent()
+		{
+			getComponent<T>() = T();
+		}
+
+	private:
+		friend class EntityManager;
+
+		Entity(const size_t& id);
+
+		size_t m_Id;
+		bool m_Active;
+
+		ComponentTuple m_Components;
+	};
+}
