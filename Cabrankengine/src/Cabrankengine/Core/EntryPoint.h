ba4a65cd8430bbd2aa8316f9de4bd5b1c343129a
cabranca <joaquincabrera98@gmail.com>
2025-08-12 00:07:23 -0300

    Handle preprocessor guard in Entry Point TODO

--- Cabrankengine/src/Cabrankengine/Core/EntryPoint.h
+++ Cabrankengine/src/Cabrankengine/Core/EntryPoint.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// TODO: I don't think we actually need this. I mean, if the platform is not supported this cannot even run (I think)
+// Despite being obvious, this is a good practice to be able to give a clear error message if the platform is not supported.
 #if defined(CE_PLATFORM_WINDOWS) || defined(CE_PLATFORM_LINUX)
 
 // TODO: understand the "extern" keyword. I think the idea is to make a "forward declaration" of the function
@@ -16,4 +16,6 @@ int main(int argc, char** argv)
 	delete app;
 }
 
+#else
+	#error "Cabrankengine only supports Windows and Linux!"
 #endif
