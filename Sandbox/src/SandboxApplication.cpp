e97e30c37ec1959eaa8fc76039e8f19613308add
cabranca <joaquincabrera98@gmail.com>
2025-08-13 11:44:35 -0300

    Update Sandbox Application

--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -1,11 +1,15 @@
 #include <Cabrankengine.h>
 
+// --- Entry Point ---
+#include "Cabrankengine/Core/EntryPoint.h"
+
 #include <Cabrankengine/Events/KeyEvent.h>
 #include <Platform/OpenGL/OpenGLShader.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <imgui.h>
 #include <Cabrankengine/Renderer/Texture.h>
+#include "Sandbox2D.h"
 
 class ExampleLayer : public cabrankengine::Layer {
 public:
@@ -86,7 +90,8 @@ private:
 class Sandbox : public cabrankengine::Application {
 public:
 	Sandbox() {
-		pushLayer(new ExampleLayer());
+		//pushLayer(new ExampleLayer());
+		pushLayer(new Sandbox2D());
 	}
 	~Sandbox() {
 
