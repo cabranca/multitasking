fe2baa0c39efffd7f41804508d8276ee5de5289b
cabranca <joaquincabrera98@gmail.com>
2025-08-17 19:21:16 -0300

    Update shader with batch rendering

--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -2,17 +2,25 @@
 #version 330 core
 			
 layout(location = 0) in vec3 pos;
-layout(location = 1) in vec2 tex;
+layout(location = 1) in vec4 col;
+layout(location = 2) in vec2 tex;
+layout(location = 3) in float texIndex;
+layout(location = 4) in float tilingFactor;
 
 uniform mat4 u_ViewProjection;
-uniform mat4 u_Transform;
 
 out vec2 v_TexCoord;
+out vec4 v_Color;
+out float v_TexIndex;
+out float v_TilingFactor;
 
 void main()
 {
 	v_TexCoord = tex;
-	gl_Position = u_ViewProjection * u_Transform * vec4(pos, 1.0);
+	v_Color = col;
+	v_TexIndex = texIndex;
+	v_TilingFactor = tilingFactor;
+	gl_Position = u_ViewProjection * vec4(pos, 1.0);
 }
 
 #type fragment
@@ -20,13 +28,14 @@ void main()
 			
 layout(location = 0) out vec4 color;
 
-uniform vec4 u_Color;
-uniform float u_TilingFactor;
+in vec4 v_Color;
 in vec2 v_TexCoord;
+in float v_TexIndex;
+in float v_TilingFactor;
 
-uniform sampler2D u_Texture;
+uniform sampler2D u_Textures[32];
 
 void main()
 {
-	color = texture(u_Texture, v_TexCoord * u_TilingFactor) * u_Color;
+	color = texture(u_Textures[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
 }
