fc2a905f76b7ca3a650b270e886067874815d3f5
cabranca <joaquincabrera98@gmail.com>
2025-08-17 19:21:27 -0300

    Update Sandbox2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -30,6 +30,7 @@ void Sandbox2D::onUpdate(cabrankengine::Timestep delta) {
 	m_CameraController.onUpdate(delta);
 
 	// Render
+	Renderer2D::resetStats();
 	{
 		CE_PROFILE_SCOPE("Renderer Prep");
 		RenderCommand::setClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -37,14 +38,27 @@ void Sandbox2D::onUpdate(cabrankengine::Timestep delta) {
 	}
 
 	{
+		static float rotation = 0.f;
+		rotation += delta * 50.f;
+
 		CE_PROFILE_SCOPE("Renderer Draw");
 		Renderer2D::beginScene(m_CameraController.getCamera());
 
-		Renderer2D::drawQuad({ -1.f, 0.f }, { 0.8f, 0.8f }, m_SquareColor);
+		Renderer2D::drawRotatedQuad({ -1.f, 0.f }, { 0.8f, 0.8f }, rotation, m_SquareColor);
 		Renderer2D::drawQuad({ 0.5f, 0.5f }, { 0.5f, 0.75f }, m_SquareColorVariant);
-		Renderer2D::drawRotatedQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, 45.0f, m_CheckerboardTexture, 10.0f, vec4(1.0, 0.9f, 0.9f, 1.0f));
+		Renderer2D::drawQuad({ -5.f, -5.f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Renderer2D::drawRotatedQuad({ -5.f, -5.f, -0.1f }, { 1.0f, 1.0f }, 45.0f, m_CheckerboardTexture, 20.0f);
 
 		Renderer2D::endScene();
+
+		/*Renderer2D::beginScene(m_CameraController.getCamera());
+		for (float y = -5.f; y < 5.f; y += 0.5f) {
+			for (float x = -5.f; x < 5.f; x += 0.5f) {
+				glm::vec4 color = { (x + 5.f) / 10.f, 0.4f, (y + 5.f) / 10.f, 1.0f };
+				Renderer2D::drawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
+		Renderer2D::endScene();*/
 	}
 }
 
@@ -52,8 +66,14 @@ void Sandbox2D::onImGuiRender() {
 	CE_PROFILE_FUNCTION();
 	ImGui::Begin("Settings");
 
+	auto stats = Renderer2D::getStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.getTotalVertexCount());
+	ImGui::Text("Indices %d:", stats.getTotalIndexCount());
+
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
-	ImGui::ColorEdit4("Square Variant Color", glm::value_ptr(m_SquareColorVariant));
 
 	ImGui::End();
 }
